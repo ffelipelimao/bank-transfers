ca063a0d9d23c49a73b0548dfb399a53d68af17a
Luiz Felipe Limao <lflimao@hotmail.com>
2023-11-15 14:19:26 -0300

    fist

---
+++ src/test/java/com/bank/transfersapi/TransfersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.transfersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
